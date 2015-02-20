2562e5d1215b9d26402339563c55ec6c1b4a6f44
Laurent Vaylet <laurent.vaylet@gmail.com>
2015-02-20 14:13:45 +0100

    Males all perish

--- main.R
+++ main.R
@@ -15,3 +15,18 @@ test$Survived = rep(0, nrow(test))
 # Extract PassengerId and Survived for submission
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "theyallperish.csv", row.names = FALSE)
+
+# Now train a model based on gender and age ("women and children first")
+summary(train$Sex)
+
+# What proportion of males and females survived?
+prop.table(table(train$Sex, train$Survived),1)
+# 74% of females survived, compared to 19% of males
+
+# So now assume only females survived
+test$Survived <- 0
+test$Survived[test$Sex == "female"] <- 1
+
+# Extract PassengerId and Survived for submission
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "malesallperish.csv", row.names = FALSE)
